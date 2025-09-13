31dea1407b47635d2f0b91a5b13e3b3f27422197
atomvn <hnvanhao@gmail.com>
2025-09-13 15:23:39 +0700

    Update udp port opened by PX4 on my os

--- src/main.py
+++ src/main.py
@@ -6,26 +6,26 @@ os.environ["PX4_HOME_LON"] = "105.792958"  # export PX4_HOME_LON=105.72807953895
 
 PROTO = "udp"
 SERVER_HOST = ""
-DEFAULT_PORT = 50060
+DEFAULT_PORT = 18571
 DEFAULT_BIND_PORT = 14541
 
 commands = []
 
 # run gazebo
 # cspell: ignore mavsdk mavlink routerd sitl lunp
-
+# /dev/ttyACM0:57600 
 
 def run_commands():
 
     commands = [
         "dependencies/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_multiple_run.sh -n 6 -m iris",
-        # " mavlink-routerd -t 5761 /dev/ttyACM0:57600 -e 127.0.0.1:14541 -e 127.0.0.1:14551",
-        # f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 0} -p {DEFAULT_PORT + 0}",
-        # f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 1} -p {DEFAULT_PORT + 1}",
-        # f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 2} -p {DEFAULT_PORT + 2}",
-        # f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 3} -p {DEFAULT_PORT + 3}",
-        # f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 4} -p {DEFAULT_PORT + 4}",
-        # f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 5} -p {DEFAULT_PORT + 5}",
+        " mavlink-routerd -t 5761 -e 127.0.0.1:14541 -e 127.0.0.1:14551",
+        f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 0} -p {DEFAULT_PORT + 0}",
+        f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 1} -p {DEFAULT_PORT + 1}",
+        f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 2} -p {DEFAULT_PORT + 2}",
+        f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 3} -p {DEFAULT_PORT + 3}",
+        f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 4} -p {DEFAULT_PORT + 4}",
+        f"python3 src/utils/mavsdk_server_shell.py {PROTO}://{SERVER_HOST}:{DEFAULT_BIND_PORT + 5} -p {DEFAULT_PORT + 5}",
         "python3 src/app.py",
         # "python3 src/map/DG5.py",
         # "dependencies/QGroundControl.AppImage",
