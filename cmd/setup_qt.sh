423d3c45a3fada812dc3df5f12609ce0a338819a
atomvn <hnvanhao@gmail.com>
2025-10-01 21:56:30 +0700

    Update Rescue_Map Page

--- cmd/setup_qt.sh
+++ cmd/setup_qt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install python3-pyqt5 -y
+# sudo apt-get install python3-pyqt5 -y
 sudo apt-get install qttools5-dev-tools -y
 sudo apt-get install qttools5-dev -y
-sudo apt install python3-pyqt5.qtwebengine -y
+# sudo apt install python3-pyqt5.qtwebengine 
